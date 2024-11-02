c81b819f633ef522a2b9b0602b40d54882f9066b
Vijayakumar A <36878324+kumvijaya@users.noreply.github.com>
2024-11-02 11:45:26 +0530

    T6846: pr title check updated

--- scripts/check-pr-title-and-commit-messages.py
+++ scripts/check-pr-title-and-commit-messages.py
@@ -23,17 +23,9 @@ def check_commit_message(title):
         sys.exit(1)
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("Usage: {} <pull_request_url> <github_token>".format(sys.argv[0]))
+    if len(sys.argv) < 2:
+        print("Please specify pull request URL!")
         sys.exit(1)
-
-    pr_url = sys.argv[1]
-    github_token = sys.argv[2]
-
-    headers = {
-        'Authorization': f'token {github_token}',
-        'Accept': 'application/vnd.github.v3+json'
-    }
 
     # There seems to be a race condition that causes this scripts to receive
     # an incomplete PR object that is missing certain fields,
@@ -43,9 +35,7 @@ if __name__ == '__main__':
     time.sleep(5)
 
     # Get the pull request object
-    pr_response = requests.get(pr_url, headers=headers)
-    pr_response.raise_for_status()
-    pr = pr_response.json()
+    pr = requests.get(sys.argv[1]).json()
     if "title" not in pr:
         print("The PR object does not have a title field!")
         print("Did not receive a valid pull request object, please check the URL!")
---
+++ scripts/check_regex.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Checks given input string matches the given regex'
+)
+parser.add_argument(
+    '-i',
+    '--input',
+    required=True,
+    help='Provide input string e.g. T1234: Ipsec input changed')
+parser.add_argument(
+    '-r',
+    '--regex',
+    required=True,
+    help='Provide regex to match id e.g. ^(([a-zA-Z0-9\-_.]+:\s)?)T\d+:\s+[^\s]+.*')
+
+args = parser.parse_args()
+input = args.input
+regex = args.regex
+
+if __name__ == '__main__':
+    output = 'true' if re.match(regex, input) else 'false'
+    print(output)
